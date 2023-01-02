cfbb1d87c64ab7926f78a2e8cb0947e7d6d8abdc
August McAllister <august.chm@gmail.com>
2023-01-01 22:36:13 -0600

    Updated user registration to account for all required fields

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -18,13 +18,13 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {
-  const username = req.body.username;
+  const { username, phone_number, email_address } = req.body;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const queryText = `INSERT INTO "user" (username, password, phone_number, email_address, subscription_default)
+    VALUES ($1, $2, $3, $4, 'false') RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [username, password, phone_number, email_address])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
