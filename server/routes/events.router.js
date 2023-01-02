f9e303ed684af6ea4b1141511930455663b1ec26
August McAllister <august.chm@gmail.com>
2023-01-01 22:23:04 -0600

    Added events route

--- server/routes/events.router.js
+++ server/routes/events.router.js
@@ -1,17 +1,8 @@
 const express = require('express');
+const { rejectUnathenticated } = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
-});
-
-/**
- * POST route template
- */
 router.post('/', (req, res) => {
   // POST route code here
 });
