e76381296ad88ac6907646fb3d1b76e1c5b70c3f
August McAllister <august.chm@gmail.com>
2023-01-02 10:11:16 -0600

    feat: added route to post new event

--- server/routes/events.router.js
+++ server/routes/events.router.js
@@ -15,8 +15,29 @@ router.get('/byHost', (request, response) => {
   } else { response.sendStatus(401) }
 })
 
-router.post('/', (request, response) => {
-  // POST route code here
+router.post('/newEvent', (request, response) => {
+
+  if (request.isAuthenticated()) {
+
+    const { name, date, time, location, description, ticket_link, visible } = request.body;
+    const host_id = request.user.id;
+    
+    const queryText = `
+    INSERT INTO event
+      (name, date, time, location, description, ticket_link, visible, host_id)
+    VALUES
+      ($1, $2, $3, $4, $5, $6, $7, $8)
+    ;`;
+
+    pool
+      .query(
+        queryText,
+        [ name, date, time, location, description, ticket_link, visible, host_id ]
+      )
+      .then(databaseResponse => { console.log('New event posted'); response.sendStatus(201) })
+      .catch(err => { console.log('Error in POST /newEvent', err); response.sendStatus(500) })
+    
+  }
 });
 
 module.exports = router;
