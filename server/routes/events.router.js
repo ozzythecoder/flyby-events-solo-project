f4f28722e2f5b00d8cdd51708e12b6388592d89e
August McAllister <august.chm@gmail.com>
2023-01-01 22:35:57 -0600

    Added route to test protected database query

--- server/routes/events.router.js
+++ server/routes/events.router.js
@@ -3,7 +3,19 @@ const { rejectUnathenticated } = require('../modules/authentication-middleware')
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.get('/byHost', (request, response) => {
+
+  if (request.isAuthenticated()) {
+    pool
+      .query(`SELECT * FROM event WHERE host_id = $1`, [request.user.id])
+      .then(databaseResponse => {
+        response.send(databaseResponse.rows)
+      })
+      .catch(err => { console.log('Error in GET /byHost', err); response.sendStatus(500) })
+  } else { response.sendStatus(401) }
+})
+
+router.post('/', (request, response) => {
   // POST route code here
 });
 
