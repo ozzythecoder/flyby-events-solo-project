ce39061de22355e7450fa2d847a6260c61d1fd14
August McAllister <august.chm@gmail.com>
2023-01-04 10:19:45 -0600

    feat: added test reducer to store all events

--- src/components/AllEvents/AllEvents.jsx
+++ src/components/AllEvents/AllEvents.jsx
@@ -6,7 +6,7 @@ export default function AllEvents() {
 
   return (
     <div>
-
+      
     </div>
   )
 }
---
+++ src/redux/reducers/events.reducer.js
@@ -0,0 +1,8 @@
+const eventsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_ALL_EVENTS': return action.payload;
+    default: return state;
+  }
+}
+
+export default eventsReducer;
