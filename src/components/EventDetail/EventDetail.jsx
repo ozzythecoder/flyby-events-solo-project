bc942e98a69e207052980a38fdb853cf594e6c04
August McAllister <august.chm@gmail.com>
2023-01-04 17:46:07 -0600

    feat: rendering event details to page; renders "sorry" message if event visibility is private

--- src/components/EventDetail/EventDetail.jsx
+++ src/components/EventDetail/EventDetail.jsx
@@ -3,12 +3,34 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 export default function EventDetail() {
-
+  const dispatch = useDispatch();
   const { eventID } = useParams();
+
+  const thisEvent = useSelector((store) =>
+    store.events.filter((el) => el.id == eventID)
+  );
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_MY_EVENTS" });
+  }, []);
 
   return (
     <div>
-      In page for event ID {eventID}
+      <p onClick={() => window.history.back()}>
+        <b>Go Back</b>
+      </p>
+      {thisEvent.map((event, index) => {
+        return event.visible ? (
+          <div>
+            <p>{event.name}</p>
+            <p>{event.date}</p>
+            <p>{event.time}</p>
+            <p>{event.location}</p>
+            <p>{event.description}</p>
+            <p>{event.ticket_link || 'none'}</p>
+          </div>
+        ) : (<>Sorry, you do not have access to this event.</>)
+      })}
     </div>
-  )
+  );
 }
