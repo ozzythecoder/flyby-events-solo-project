3226091dc706fa81b23ed820118b0900493448f4
August McAllister <august.chm@gmail.com>
2023-01-09 11:50:25 -0600

    feat: added card components for events; adjusted global link styling

--- src/components/MyEvents/MyEvents.jsx
+++ src/components/MyEvents/MyEvents.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+import { Card, CardContent, Typography } from "@mui/material";
+
 export default function AllEvents() {
   const dispatch = useDispatch();
 
   const myEvents = useSelector((store) => store.myEvents);
-  const user = useSelector(store => store.user)
+  const user = useSelector((store) => store.user);
 
   useEffect(() => {
     dispatch({ type: "FETCH_ALL_EVENTS" });
@@ -14,28 +16,36 @@ export default function AllEvents() {
 
   return (
     <div>
-      <h2>My Events</h2>
+      <Typography variant="h4" sx={{ m: 2 }}>
+        My Events
+      </Typography>
       <div>
         {myEvents.map((event, index) => {
           return (
             <div key={index}>
-              <p>
-                <a href= {`/#/event/${event.id}`}>
-
-                <b>{event.name}</b></a>
-
-                {user.id === event.host_id && ' - Hosting'}
-              </p>
-              <p>When: {event.date}</p>
-              <p>Where: {event.location}</p>
-              <p>Description:<br />{event.description}</p>
+              <Card sx={{ mb: 2, mx: 2 }} variant="outlined">
+                <CardContent>
+                  <Typography variant="h5">
+                    <a href={`/#/event/${event.id}`}>{event.name}</a>
+                    {user.id === event.host_id && " - Hosting"}
+                  </Typography>
+                  <Typography variant="body1">
+                    When: {event.date}
+                  </Typography>
+                  <Typography variant="body1">
+                  Where: {event.location}
+                  </Typography>
+                  <Typography variant="body1">
+                    Description: <br />
+                    {event.description}
+                  </Typography>
+                </CardContent>
+              </Card>
             </div>
           );
         })}
       </div>
-      <div>
-
-      </div>
+      <div></div>
     </div>
   );
 }
