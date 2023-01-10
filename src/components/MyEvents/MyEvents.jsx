22ea99a0d37b8d8bed693678db7376eb86d2362b
August McAllister <august.chm@gmail.com>
2023-01-10 15:33:18 -0600

    refactor: componentized event display from MyEvents into MyEventsItem

--- src/components/MyEvents/MyEvents.jsx
+++ src/components/MyEvents/MyEvents.jsx
@@ -3,10 +3,14 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { Card, CardContent, Typography } from "@mui/material";
 
-export default function MyEvents({ stateFilter = 'all', title = "My Events", hosting = false}) {
-  const dispatch = useDispatch();
+import MyEventsItem from "../MyEventsItem/MyEventsItem";
 
-  // utility object to filter for certain events
+export default function MyEvents({ stateFilter = 'all', title = "My Events" }) {
+  const dispatch = useDispatch();
+  
+  const user = useSelector((store) => store.user);
+  
+  // utility object to filter for events by stateFilter
   const eventFilters = {
     all: () => true,
     pending: event => event.guest_state === 'pending',
@@ -14,7 +18,7 @@ export default function MyEvents({ stateFilter = 'all', title = "My Events", hos
     hosting: event => event.host_id === user.id
   }
   
-  const user = useSelector((store) => store.user);
+  // filter events based on stateFilter
   const myEvents = useSelector(
     store => store.events.myEvents.filter(eventFilters[stateFilter])
   );
@@ -28,36 +32,7 @@ export default function MyEvents({ stateFilter = 'all', title = "My Events", hos
       <Typography variant="h4" sx={{ m: 2 }}>
         {title}
       </Typography>
-      <div>
-        {myEvents.map((event, index) => {
-          return (
-            <div key={event.id}>
-              <a href={`/#/event/${event.id}`}>
-                <Card sx={{ mb: 2, mx: 2 }} variant="outlined">
-                  <CardContent>
-                    <Typography variant="h5">
-                      {event.name}
-
-                      {user.id === event.host_id && " - Hosting"}
-                    </Typography>
-                    <Typography variant="body1">
-                      {/* ⚠️ FORMAT WITH LUXON.JS */}
-                      When: {new Date(event.date).toLocaleDateString()}
-                    </Typography>
-                    <Typography variant="body1">
-                      Where: {event.location}
-                    </Typography>
-                    <Typography variant="body1" sx={{ mt: 1 }}>
-                      {event.description}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </a>
-            </div>
-          );
-        })}
-      </div>
-      {/* {JSON.stringify(useSelector(store => store.events.myEvents))} */}
+      {myEvents.map(event => <MyEventsItem key={event.id} event={event} /> )}
     </div>
   );
 }
