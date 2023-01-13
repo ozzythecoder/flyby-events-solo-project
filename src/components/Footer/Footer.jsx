96a64e2860017d93002421a07d34475fe51fa571
August McAllister <august.chm@gmail.com>
2023-01-13 16:35:26 -0600

    feat: footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
-import "./Footer.css";
-
 import { Box } from "@mui/material";
 
 function Footer() {
+
+  const footerStyle = {
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    p: 2,
+    backgroundColor: "rgb(255, 255, 255, 0.85)",
+  }
+
   return (
-    <Box sx={{ position: 'fixed', bottom: 0, width: '100%', p: 2, backgroundColor: 'white' }}>
+    <Box sx={footerStyle}>
       <center>&copy; August McAllister</center>
     </Box>
   );
