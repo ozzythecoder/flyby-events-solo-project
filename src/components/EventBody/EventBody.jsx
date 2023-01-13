ac91b9d0487061dd47d85ce2f13932d92fbfaa39
August McAllister <august.chm@gmail.com>
2023-01-13 13:38:10 -0600

    feat: apply date formatting to My Events & event detail views

--- src/components/EventBody/EventBody.jsx
+++ src/components/EventBody/EventBody.jsx
@@ -1,6 +1,17 @@
 import { Typography } from "@mui/material";
+import { DateTime } from "luxon";
+
 
 export default function EventBody({ event }) {
+
+  const date =
+    DateTime.fromISO(event.date)
+      .toLocaleString(DateTime.DATE_FULL)
+
+  const time = DateTime.fromISO(event.time)
+    .toLocaleString(DateTime.TIME_WITH_SHORT_OFFSET)
+
+
   return (
     <div>
       <Typography variant="h3">
@@ -9,8 +20,8 @@ export default function EventBody({ event }) {
       <Typography variant="subheading" sx={{ mb: 1 }}>
         {!event.visible && "This is a private event."}
       </Typography>
-      <Typography variant="body1">{event.date}</Typography>
-      <Typography variant="body1">{event.time}</Typography>
+      <Typography variant="body1">{date}</Typography>
+      <Typography variant="body1">{time}</Typography>
       <Typography variant="body1">{event.location}</Typography>
       <Typography variant="body1">{event.ticket_link || ""}</Typography>
       <Typography variant="body1">{event.description}</Typography>
