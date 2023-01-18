a9393e65cc6d53f150e043261fa9a61f12b8eb90
August McAllister <august.chm@gmail.com>
2023-01-17 20:40:19 -0600

    feat: updated database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,44 @@
+----- USER TABLE
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    id SERIAL PRIMARY KEY,
+    username character varying(255) NOT NULL UNIQUE,
+    password character varying(255) NOT NULL,
+    phone_number numeric(10,0),
+    email_address character varying(255) NOT NULL,
+    subscription_default boolean NOT NULL DEFAULT false,
+    profile_img_url character varying(255)
 );
+
+CREATE UNIQUE INDEX user_pk ON "user"(id int4_ops);
+CREATE UNIQUE INDEX user_username_key ON "user"(username text_ops);
+
+------ EVENT TABLE
+
+CREATE TABLE event (
+    id SERIAL PRIMARY KEY,
+    name character varying(255) NOT NULL,
+    date date NOT NULL,
+    time time with time zone NOT NULL,
+    location character varying(255),
+    description character varying(400) NOT NULL,
+    ticket_link character varying(255),
+    visible boolean NOT NULL,
+    host_id integer NOT NULL REFERENCES "user"(id)
+);
+
+CREATE UNIQUE INDEX event_pk ON event(id int4_ops);
+
+------ USER/EVENT JUNCTION TABLE
+
+CREATE TYPE guest_state AS ENUM ('subscribed', 'added', 'pending');
+
+CREATE TABLE user_event (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
+    event_id integer NOT NULL REFERENCES event(id) ON DELETE CASCADE,
+    guest_state guest_state NOT NULL DEFAULT 'pending'::guest_state,
+    is_read boolean NOT NULL DEFAULT false
+);
+
+CREATE UNIQUE INDEX user_event_pk ON user_event(id int4_ops);
